fe5593f35e8f535e29ba3dda9cbdd56af039222b
Sagnik Bhattacharya <sagnikbhattacharya1506@gmail.com>
2025-07-10 12:00:07 +0530

    Project migration June 2025

--- android/app/src/main/kotlin/com/codingliquids/navigations_complete/MainActivity.kt
+++ android/app/src/main/kotlin/com/codingliquids/navigations_complete/MainActivity.kt
@@ -2,5 +2,4 @@ package com.codingliquids.navigations_complete
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
